518ccbd6226257f26dbf38aee5627f69a7ee9e0f
Jeremy Auvray <jeremyauvray@MacBook-Neo-de-Jeremy.local>
2026-04-02 09:43:58 +0200

    revert: add loading page for nextjs server

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderCircle } from 'lucide-react'
-
-export default function Loading() {
-    return (
-        <main className="w-full h-screen flex items-center justify-center">
-            <LoaderCircle className="text-primary w-14 h-14 animate-spin" />
-        </main>
-    )
-}
